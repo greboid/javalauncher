b1fc01cbc9129b3202c9c7bf83d1415430c7ce5a
Greg Holmes <git@greg.holmes.name>
2014-03-20 10:27:07 +0000

    Add git version to header file

--- src/config/ConfigDefaults.h
+++
@@ -1,4 +0,0 @@
-#define APPLICATION_NAME "DMDirc"
-#define APPLICATION_MAIN "com\\dmdirc\\Main"
-#define APPLICATION_SETUP "com\\dmdric\\LauncherUtils"
-#define APPLICATION_PATH "c:\\Program;Files\\DMDirc\\"
