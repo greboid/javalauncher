f4608d870ed80cf6b1d28309266c40cd990c6bb2
Greg Holmes <git@greg.holmes.name>
2014-05-04 23:14:15 +0100

    Add logging to single instance.

--- src/singleinstance/SingleInstance.cpp
+++ src/singleinstance/SingleInstance.cpp
@@ -1,3 +1,4 @@
+#include "../log4z/log4z.h"
 #include "SingleInstance.h"
 
 #define SINGLEINSTANCE FALSE
@@ -10,23 +11,33 @@ SingleInstance::SingleInstance(ConfigReader& config) {
 }
 
 SingleInstance::~SingleInstance() {
+	LOGD("Destroying single instance.");
     if (instanceMutex) {
+		LOGD("Stopping instance.");
         stopped();
     }
 }
 
 bool SingleInstance::getCanStart() {
+	LOGD("Checking if we should use single instance.");
 	if (config.getBoolValue(SETTING, SINGLEINSTANCE)) {
+		LOGD("Creating single instance.");
         instanceMutex = CreateMutex(NULL, true, TEXT("DMDirc"));
         if (instanceMutex && GetLastError() == ERROR_ALREADY_EXISTS) {
-            CloseHandle(instanceMutex);
+			LOGD("Single instance exists, we should not start.");
+			stopped();
             return false;
         }
     }
+	LOGD("Single instance not required or set, we should start.");
+	stopped();
     return true;
 }
 
 void SingleInstance::stopped() {
+	LOGD("Releasing mutex.");
     ReleaseMutex(instanceMutex);
+	LOGD("Stopping mutex.");
+	CloseHandle(instanceMutex);
 }
 
