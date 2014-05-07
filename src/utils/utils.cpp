cde700c7969f7a82e547df0c032abd6969bb61db
Greg Holmes <git@greg.holmes.name>
2014-05-07 21:38:35 +0100

    Simplify ws2s

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -58,12 +58,7 @@ std::vector<std::string> Utils::splitString(std::string input, std::string delim
 }
 
 std::string Utils::ws2s(std::wstring s) {
-	int len;
-	int slength = (int)s.length();
-	len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0);
-	std::string r(len, '\0');
-	WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, &r[0], len, 0, 0);
-	return r;
+	return string(s.begin(), s.end());
 }
 
 std::string Utils::GetAppDataDirectory() {
