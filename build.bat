ac70e0bce8843b55ff12869d631b119e577264cc
Greg Holmes <git@greg.holmes.name>
2014-05-11 17:44:46 +0100

    Start working on 64bit build
    Stop supporting in source builds.

--- build.bat
+++ build.bat
@@ -4,8 +4,15 @@ GOTO JAVAHOMEEXISTS
 :SETJAVAHOME
 	JAVA_HOME="C:\Program Files\Java\jre8"
 :JAVAHOMEEXISTS
-if EXIST "build" (
-    rmdir /s/q build
+if EXIST "build-x86" (
+    rmdir /s/q build-x86
 )
-mkdir build && cd build && cmake -G"NMake Makefiles" .. && nmake
+if EXIST "build-x64" (
+    rmdir /s/q build-x64
+)
+mkdir build-x86
+mkdir build-x64
+cd build-x86 && cmake -G"Visual Studio 12" .. && msbuild.exe ALL_BUILD.vcxproj /p:Configuration=Release
+cd ..
+cd build-x64 && cmake -G"Visual Studio 12 Win64" .. && msbuild.exe ALL_BUILD.vcxproj /p:Configuration=Release
 cd ..
