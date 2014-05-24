fda3c891f1e95c2bf35481928c28e1be865012c8
Greg Holmes <git@greg.holmes.name>
2014-05-24 22:28:07 +0100

    Add initial unit testing support

--- build.bat
+++ build.bat
@@ -4,12 +4,6 @@ GOTO JAVAHOMEEXISTS
 :SETJAVAHOME
 	JAVA_HOME="C:\Program Files\Java\jre8"
 :JAVAHOMEEXISTS
-if EXIST "build-x86" (
-    rmdir /s/q build-x86
-)
-if EXIST "build-x64" (
-    rmdir /s/q build-x64
-)
 mkdir build-x86
 mkdir build-x64
 cd build-x86 && cmake -G"Visual Studio 12" .. && msbuild.exe ALL_BUILD.vcxproj /p:Configuration=Release
---
+++ test/main.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(main_test, null_test) {
+	EXPECT_EQ(1, 1);
+}
